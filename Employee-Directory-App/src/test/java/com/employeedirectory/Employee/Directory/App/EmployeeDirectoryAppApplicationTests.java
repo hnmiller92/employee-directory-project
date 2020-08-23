1bf160cb3411cddbd1034b9d1b57c22570651cee
Hannah Miller <hnmiller92@gmail.com>
2020-08-23 15:50:44 -0500

    Spring Boot Set Up

---
+++ Employee-Directory-App/src/test/java/com/employeedirectory/Employee/Directory/App/EmployeeDirectoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeedirectory.Employee.Directory.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDirectoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
